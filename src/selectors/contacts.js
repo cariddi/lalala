e2f5b36c1068d2cccbc8adcd5e0855fea2ef58d8
lucas <lucascariddi@amalgama.co>
2021-04-14 17:02:51 -0300

    removing commented code

--- src/selectors/contacts.js
+++ src/selectors/contacts.js
@@ -18,28 +18,3 @@ export const getContactDataSelector = createSelector(
         return contacts.contacts.filter( c => c.id == contactID )[ 0 ];
     }
 );
-
-// let fakeState = {
-//     contacts: [
-//         {
-//             "id": 5,
-//             "name": "Susana Córdova Pabón",
-//             "email": "ariadna_ruiz@abrego.es",
-//             "phone1": "972853428",
-//             "phone2": "631067997",
-//             "birthdate": "1995-08-10",
-//             "address": "Chalet Mónica Perea 7 Esc. 483",
-//             "state": {
-//                 "id": 29,
-//                 "name": "Bahia"
-//             },
-//             "country": {
-//                 "id": 3,
-//                 "name": "Brasil"
-//             },
-//             "avatar": null
-//         }
-//     ]
-// }
-
-// console.log(getContactsKeysSelector(fakeState))
